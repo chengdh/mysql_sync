71c765a0ceea5e2c8858ed1fa981e2e681e65377
chengdh <cheng.dongh@gmail.com>
2015-08-04 15:57:21 +0800

    修改了自动生成报警数据的用户id

--- sync_gps_alm.sql
+++ sync_gps_alm.sql
@@ -1,13 +1,13 @@
 INSERT INTO  nj_oil_warn
 (gis_id,machine_id,gps_code,service_time,reptime,gps_time,longitude,latitude,direction,speed,mileage,flags,in_date,in_user,status)
-SELECT alm.id,m.id,epid,NOW(),reptime,gpstime,longitude,latitude,direction,speed,mileage,flags,NOW(),1,0 FROM nj_machine m,gis_alm alm
+SELECT alm.id,m.id,epid,NOW(),reptime,gpstime,longitude,latitude,direction,speed,mileage,flags,NOW(),3,0 FROM nj_machine m,gis_alm alm
 WHERE m.gps_code = alm.epid
 AND alm.type=5
 AND alm.id NOT IN (SELECT gis_id FROM nj_oil_warn);
 
 INSERT INTO  nj_repair_warn
 (gis_id,machine_id,gps_code,service_time,reptime,gps_time,longitude,latitude,direction,speed,mileage,flags,in_date,in_user,status)
-SELECT alm.id,m.id,epid,NOW(),reptime,gpstime,longitude,latitude,direction,speed,mileage,flags,NOW(),1,0 FROM nj_machine m,gis_alm alm
+SELECT alm.id,m.id,epid,NOW(),reptime,gpstime,longitude,latitude,direction,speed,mileage,flags,NOW(),3,0 FROM nj_machine m,gis_alm alm
 WHERE m.gps_code = alm.epid
 AND alm.type=4
 AND alm.id NOT IN (SELECT gis_id FROM nj_oil_warn);
