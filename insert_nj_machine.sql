7a138d10f3ddb52eed5991cac832d285f070fd9c
chengdh <cheng.dongh@gmail.com>
2016-05-21 22:06:32 +0800

    修改了农机数据同步sql

--- insert_nj_machine.sql
+++ insert_nj_machine.sql
@@ -1,3 +1,3 @@
-INSERT INTO nj_machine (own_depmt_id,gps_code,in_user,in_date,bank_no,status)
-select 1,epid,3,NOW(),bankno,1 FROM gis_ep
+INSERT INTO nj_machine (own_depmt_id,gps_code,in_user,in_date,bank_no,status,vehicle_num,owner_name)
+select 2,epid,3,NOW(),bankno,1,epid,epid FROM gis_ep
 WHERE epid not in (SELECT gps_code from nj_machine);
