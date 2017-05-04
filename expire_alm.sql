3d50acb3b7ca70e8230dcf9c93dfc1850c0d9b65
chengdh <cheng.dongh@gmail.com>
2017-05-04 17:07:06 +0800

    修改了数据同步设置 去除了alm的同步

--- expire_alm.sql
+++ expire_alm.sql
@@ -1,3 +1,3 @@
---超过1天的情况下,将所有未处理的报警数据设置为过期
+-- 超过1天的情况下,将所有未处理的报警数据设置为过期
 UPDATE nj_oil_warn SET status = 4 WHERE status = 0 AND DATE_ADD(service_time,INTERVAL 1 DAY) < NOW();
 UPDATE nj_repair_warn SET status = 4 WHERE status = 0 AND DATE_ADD(service_time,INTERVAL 1 DAY) < NOW();
